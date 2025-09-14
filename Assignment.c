2be75cdad2b964011dd7bfbe857f18983a2a48e1
Charis-Opol <charisopol@gmail.com>
2025-09-14 17:06:31 +0300

    1st commit

---
+++ Assignment.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int array[5] = {55, 22, 3, 445, 52};
+
+    int max_index = 0; 
+    for (i = 1; i < 5; i++){
+        if (array[max_index] < array[i]){
+            max_index = i;
+        }
+    }
+
+    printf("The maximum value is: %d\n", array[max_index]);
+    return 0;
+}
+
