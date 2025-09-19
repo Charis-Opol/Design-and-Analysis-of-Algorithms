3fcd9711563477613eb4e0b7d5fd874009f8dcfc
Charis-Opol <charisopol@gmail.com>
2025-09-19 23:25:25 +0300

    2nd commit

--- Assignment.c
+++ Assignment.c
@@ -4,7 +4,7 @@ int main(){
     int array[5] = {55, 22, 3, 445, 52};
 
     int max_index = 0; 
-    for (i = 1; i < 5; i++){
+    for (int i = 1; i < 5; i++){
         if (array[max_index] < array[i]){
             max_index = i;
         }
